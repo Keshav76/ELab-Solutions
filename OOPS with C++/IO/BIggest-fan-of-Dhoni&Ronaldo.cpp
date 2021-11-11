f1b41b50cb1fb963c7aeaf38bb7f2facfb773cbb
sequel-tj <74481733+sequel-tj@users.noreply.github.com>
2021-11-12 01:33:49 +0530

    Johan

---
+++ OOPS with C++/IO/BIggest-fan-of-Dhoni&Ronaldo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int fannumber;
+    cin>>fannumber;
+    
+    if(fannumber>7) cout<<"Fan of Dhoni";
+    else if(fannumber<7) cout<<"Fan of Ronaldo";
+    else cout<<"Fan of Both Dhoni and Ronaldo";
+
+	return 0;
+}
