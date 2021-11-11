9db75b17980dbb535e12d39e87d18fab19d803e7
sequel-tj <74481733+sequel-tj@users.noreply.github.com>
2021-11-12 01:02:23 +0530

    he got kit of n cubes

---
+++ OOPS with C++/Abstract Class & Virtual Function/young-varun-birthday-today.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Gift {
+  public:virtual void Cubes()=0;  
+};
+
+class Birthday:public Gift {
+    public:void Cubes() {
+        int n; cin>>n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin>>a[i];
+        
+        for (int i = 0; i < n/2; i+=2)
+            swap(a[i],a[n-i-1]);
+        
+        for (int i = 0; i < n; i++)
+            cout<<a[i]<<" ";
+    }
+};
+
+int main()
+{
+    Birthday obj;
+    obj.Cubes();
+
+	return 0;
+}
